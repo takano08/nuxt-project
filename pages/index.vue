26f843fd91e953b48156392bf731ad72f99de4c8
takano <ri1saxxminny83xx@gmail.com>
2020-07-31 14:03:10 +0900

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+     {{ employees }}
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios')
+let url ='http://localhost:8080/employees'
+export default {
+  asyncData({params}){
+    return axios.get(url)
+      .then((res)=>{
+        return{ employees: res.data }
+      })
+
+  }
+}
+</script>
+
